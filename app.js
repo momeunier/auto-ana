a810b169e079a1fe3e0a3d24ba8b41783da85213
Marc Olivier Meunier <momeunier@gmail.com>
2024-10-08 20:57:19 +0300

    Vercel ready

--- app.js
+++ app.js
@@ -217,3 +217,11 @@ async function deleteFile(filePath) {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, "dist")));
+
+// For any other routes, serve the index.html file
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
