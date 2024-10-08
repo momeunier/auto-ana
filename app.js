6713299ee9792698d8bcac0bc00cb2bb1afb46ac
Marc Olivier Meunier <momeunier@gmail.com>
2024-10-08 22:07:28 +0300

    vercel payload size limit

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static("dist"));
 app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 const apiKey = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
